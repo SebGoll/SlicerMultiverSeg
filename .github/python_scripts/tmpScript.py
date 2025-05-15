296d2ef391b41de4a0291bd0b554682e9e455686
Sébastien Goll <sebastien.goll@kitware.com>
2025-05-15 14:27:28 +0200

    CI: Add current directory to pythonpath

--- .github/python_scripts/tmpScript.py
+++ .github/python_scripts/tmpScript.py
@@ -1,9 +1,12 @@
-
+from pathlib import Path
 
 if __name__ == '__main__':
     import sys
     import slicer
 
+    print(sys.path)
+
+    sys.path.append(Path(__file__).parent.joinpath("../..").resolve().as_posix())
     print(sys.path)
 
     slicer.util.quit()
